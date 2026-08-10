48472eebe20b88cc37875f43abdc24582e3c9281
Meghana-73966 <242ta04123@ashokacollege.in>
2026-08-10 11:15:30 +0530

    Create shift_register_tb.v

---
+++ shift_register_tb.v
@@ -0,0 +1,48 @@
+`timescale 1ns/1ps
+
+module shift_register_tb;
+
+reg clk;
+reg reset;
+reg serial_in;
+wire [3:0] q;
+
+shift_register uut (
+    .clk(clk),
+    .reset(reset),
+    .serial_in(serial_in),
+    .q(q)
+);
+
+always #5 clk = ~clk;
+
+initial begin
+    $monitor("Time=%0t | Reset=%b | Serial_in=%b | Q=%b",
+             $time, reset, serial_in, q);
+
+    clk = 0;
+    reset = 1;
+    serial_in = 0;
+
+    #10;
+    reset = 0;
+
+    serial_in = 1;
+    #10;
+
+    serial_in = 0;
+    #10;
+
+    serial_in = 1;
+    #10;
+
+    serial_in = 1;
+    #10;
+
+    serial_in = 0;
+    #10;
+
+    $finish;
+end
+
+endmodule
