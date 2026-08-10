871ef3ff723cf5d8837706749f2440b4b6a195d8
Meghana-73966 <242ta04123@ashokacollege.in>
2026-08-10 11:14:45 +0530

    Create shift_register.v

---
+++ shift_register.v
@@ -0,0 +1,19 @@
+module shift_register (
+    input clk,
+    input reset,
+    input serial_in,
+    output reg [3:0] q
+);
+
+always @(posedge clk) begin
+    if (reset)
+        q <= 4'b0000;
+    else begin
+        q[3] <= q[2];
+        q[2] <= q[1];
+        q[1] <= q[0];
+        q[0] <= serial_in;
+    end
+end
+
+endmodule
